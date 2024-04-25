cfe73a88633124995335e4b6ae9e69d19d6d9148
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2024-04-25 15:00:18 +0200

    request getAll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const serverAddress = '127.0.0.1:8081';
 
-export default App;
+const Request = () => {
+    const [data, setData] = useState(null); //data viene inizialmente settato a null, setData = funzione che ne aggiorna lo stato
+    const [loading, setLoading] = useState(true); //loading viene inizialmente settato a true, setLoading = funzione che ne aggiorna lo stato
+    const [error, setError] = useState(null);//error viene inizialmente settato a null, setError = funzione che ne aggiorna lo stato
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://' + serverAddress + "/products");
+                if (!response.ok) {
+                    throw new Error('Errore nella richiesta: ' + response.statusText);
+                }
+                const responseData = await response.json();
+                setData(responseData.data);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    console.log(data);
+    if (!data) return <div>No data available</div>;
+
+    return (
+        <div>{data.map((item, index) => (
+            <div key={index} className={"box"}>
+                <p>Nome: {item.attributes.nome}</p>
+                <p>Marca: {item.attributes.marca}</p>
+                <p>Prezzo: {item.attributes.prezzo}</p>
+            </div>
+        ))}
+        </div>
+    );
+};
+
+
+export default Request;
